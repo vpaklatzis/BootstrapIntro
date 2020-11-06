4797fd6e002c3973055e1df86eac63b52d831c4f
Vaggelis Paklatzis <vpaklatzis@gmail.com>
2020-11-06 23:34:47 +0200

    Grunt part 1

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'scss/style.css': 'scss/style.css'
+                }
+            }
+        },
+        watch: {
+            files: 'scss/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'scss/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+};
